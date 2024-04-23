b5aa775fb374207e8a9089660b057d3bf5b84c14
strelok93 <strelok0493@gmail.com>
2024-04-23 08:49:56 +0500

    sababi

--- resources/ts/components/GroupModal.vue
+++ resources/ts/components/GroupModal.vue
@@ -5,14 +5,16 @@
             <section :class="`${props.color}-scroll`" class="h-full w-full overflow-y-auto px-1 scroll ">
                <table class="w-full">
                   <tr>
-                     <td colspan="5" :class="`bg-${props.color}-600`" class="py-1 text-center">{{ props.filter }}</td>
+                     <td :colspan="props.color == 'indigo' ? 6 : 5" :class="`bg-${props.color}-600`" class="py-1 text-center">{{ props.filter }}</td>
                   </tr>
                   <tr class="border-b-4 border-zinc-900">
-                     <td class="py-1 rounded-tl px-2">№</td>
+                     <td class="py-1 px-2">№</td>
                      <td class="py-1">Transport nomi</td>
                      <td class="py-1">Kirgan vaqti</td>
                      <td class="py-1">Chiqqan vaqti</td>
-                     <td class="py-1 rounded-tr">Umumiy</td>
+                     <td class="py-1">Umumiy</td>
+                     <td class="py-1 " v-if="props.color == 'indigo'">Sababi</td>
+                     
                   </tr>
                   <tr v-for="(transport, index) in cars" class="bg-zinc-800 border-y-4 border-zinc-900">
                      <td class="py-1 px-2">{{ index + 1 }}</td>
@@ -20,6 +22,7 @@
                      <td class="py-1">{{ moment(transport.geozone_in).format('YYYY-MM-DD HH:mm') }}</td>
                      <td class="py-1">{{ moment(transport.geozone_out).format('YYYY-MM-DD HH:mm') }}</td>
                      <td class="py-1">{{ getDifference(transport) }}</td>
+                     <td class="py-1 w-52">{{ transport.cause}}</td>
                   </tr>
                </table>
             </section>
@@ -35,4 +38,5 @@ import { Transports } from '@/entities/transports/Transports'
 const props = defineProps(['filter', 'color'])
 const store = Transports()
 const cars = store.getFilterGroup(props.filter, props.color)
+
 </script>
