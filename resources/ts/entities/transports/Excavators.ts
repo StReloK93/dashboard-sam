320540a0420340a0acfee7329ad02b0141f8faca
strelok93 <strelok0493@gmail.com>
2024-11-11 16:11:12 +0500

    fast

--- resources/ts/entities/transports/Excavators.ts
+++ resources/ts/entities/transports/Excavators.ts
@@ -4,32 +4,14 @@ import { splitNumberAndText, getRandomArbitrary } from "@/helpers/timeFormat";
 
 export const Excavators = defineStore("Excavators", () => {
    const ExcavatorList = ref([]);
-   const maxprosent = getRandomArbitrary(90, 97)
-
    async function getExcavatorStates() {
       const { data } = await axios.get("api/transports/excavatorstate");
+      
       data.forEach((excavator) => {
          const { number, text } = splitNumberAndText(excavator.mexanizm_nomi);
          excavator.number = number;
          excavator.name = text;
       });
-
-
-      // test
-      const miss = Math.round(data.length / 100 * maxprosent)
-
-      const noactive = data.filter((car) => car.status_of != 'Ishda')
-      const active = data.filter((car) => car.status_of == 'Ishda')
-
-      const ineed = miss - active.length
-
-      noactive.forEach((carac, index) => {
-         if (index < ineed) {
-            const current = data.find((car) => carac.number == car.number)
-            current.status_of = 'Ishda'
-         }
-      })
-      // test
 
       ExcavatorList.value = data;
    }
