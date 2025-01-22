b8f15f1c043658f1443f43592889e942e521e85f
strelok93 <strelok0493@gmail.com>
2025-01-22 10:04:32 +0500

    fast

--- resources/ts/entities/transports/Excavators.ts
+++ resources/ts/entities/transports/Excavators.ts
@@ -6,24 +6,6 @@ export const Excavators = defineStore("Excavators", () => {
    const ExcavatorList = ref([]);
    async function getExcavatorStates() {
       const { data } = await axios.get("api/transports/excavatorstate");
-      data.push({
-         "id": "293388",
-         "mexanizm_nomi": "24-ЭКГ-5А",
-         "code_of_cause": null,
-         "cause": null,
-         "mexanizm_turi": "1-Ekskavatorlar",
-         "date_explot": "01.04.2010",
-         "cause_old": null,
-         "timeBegin": null,
-         "timeEnd": null,
-         "durability": null,
-         "typeStay": null,
-         "comment": null,
-         "status_of": "Ishda",
-         "number": 24,
-         "name": "ЭКГ-5А"
-      })
-      
       data.forEach((excavator) => {
          if (excavator.status_of == "Ta`mirda") console.log(excavator);
 
